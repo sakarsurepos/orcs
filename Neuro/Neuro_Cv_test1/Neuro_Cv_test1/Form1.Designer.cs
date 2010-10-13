5b107b87e8663c92f75f33e8f15afaf6f043177d
kamil.zidek <kamil.zidek@efad9e02-0b5d-11df-a48f-3fa99c96b54c>
2010-10-13 22:13:03 +0000

    second commit

---
+++ Neuro/Neuro_Cv_test1/Neuro_Cv_test1/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Neuro_Cv_test1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtTestInput1 = new System.Windows.Forms.TextBox();
+            this.txtTestInput2 = new System.Windows.Forms.TextBox();
+            this.lblTestOutput = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Train XOR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(174, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 94);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Test";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtTestInput1
+            // 
+            this.txtTestInput1.Location = new System.Drawing.Point(255, 29);
+            this.txtTestInput1.Name = "txtTestInput1";
+            this.txtTestInput1.Size = new System.Drawing.Size(40, 20);
+            this.txtTestInput1.TabIndex = 2;
+            this.txtTestInput1.Text = "0";
+            // 
+            // txtTestInput2
+            // 
+            this.txtTestInput2.Location = new System.Drawing.Point(311, 29);
+            this.txtTestInput2.Name = "txtTestInput2";
+            this.txtTestInput2.Size = new System.Drawing.Size(40, 20);
+            this.txtTestInput2.TabIndex = 3;
+            this.txtTestInput2.Text = "0";
+            // 
+            // lblTestOutput
+            // 
+            this.lblTestOutput.AutoSize = true;
+            this.lblTestOutput.Location = new System.Drawing.Point(285, 72);
+            this.lblTestOutput.Name = "lblTestOutput";
+            this.lblTestOutput.Size = new System.Drawing.Size(39, 13);
+            this.lblTestOutput.TabIndex = 4;
+            this.lblTestOutput.Text = "Vystup";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Neuro_Cv_test1.Properties.Resources.obr;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(142, 74);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 127);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(36, 126);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 365);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblTestOutput);
+            this.Controls.Add(this.txtTestInput2);
+            this.Controls.Add(this.txtTestInput1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtTestInput1;
+        private System.Windows.Forms.TextBox txtTestInput2;
+        private System.Windows.Forms.Label lblTestOutput;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Neuro/Neuro_Cv_test1/Neuro_Cv_test1/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NeuronDotNet.Core; //
+using NeuronDotNet.Core.Backpropagation;  //
+
+namespace Neuro_Cv_test1
+{
+    public partial class Form1 : Form
+    {
+
+        //Neuro
+        private BackpropagationNetwork xorNetwork; //
+        private double[] errorList; // 
+        private int cycles = 5000;  //
+        private int neuronCount = 3; // 
+        private double learningRate = 0.25d; //
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            errorList = new double[cycles]; //
+
+            LinearLayer inputLayer = new LinearLayer(2); //
+            SigmoidLayer hiddenLayer = new SigmoidLayer(neuronCount); //
+            SigmoidLayer outputLayer = new SigmoidLayer(1); //
+            new BackpropagationConnector(inputLayer, hiddenLayer); //
+            new BackpropagationConnector(hiddenLayer, outputLayer); //
+            xorNetwork = new BackpropagationNetwork(inputLayer, outputLayer); //
+            xorNetwork.SetLearningRate(learningRate); //
+
+            TrainingSet trainingSet = new TrainingSet(2, 1);
+            trainingSet.Add(new TrainingSample(new double[2] { 0d, 0d }, new double[1] { 0d }));
+            trainingSet.Add(new TrainingSample(new double[2] { 0d, 1d }, new double[1] { 1d }));
+            trainingSet.Add(new TrainingSample(new double[2] { 1d, 0d }, new double[1] { 1d }));
+            trainingSet.Add(new TrainingSample(new double[2] { 1d, 1d }, new double[1] { 0d }));
+
+            double max = 0d;
+
+            //Ucenie
+			xorNetwork.Learn(trainingSet, cycles);
+			double[] indices = new double[cycles];
+			for (int i = 0; i < cycles; i++) { indices[i] = i; }
+            MessageBox.Show("Training Finish");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (xorNetwork != null)
+            {
+                //TEST 
+                lblTestOutput.Text = xorNetwork.Run(new double[] { double.Parse(txtTestInput1.Text), double.Parse(txtTestInput2.Text) })[0].ToString("0.000000");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp1 = new Bitmap(pictureBox2.ClientSize.Width, pictureBox2.ClientSize.Height);
+            Rectangle rect1 = new Rectangle(0, 0, 20, 20);
+
+            Bitmap DrawArea;
+            DrawArea = new Bitmap(pictureBox2.Size.Width, pictureBox2.Size.Height);
+            pictureBox2.Image = DrawArea;
+            Graphics g;
+            g = Graphics.FromImage(DrawArea);
+            g.FillRectangle(Brushes.Black, 5, 5, 10, 10);
+            pictureBox2.DrawToBitmap(bmp1, pictureBox2.ClientRectangle);
+
+            Color pixelColor = bmp1.GetPixel(8, 8);
+            if (pixelColor.Name.ToString() == "ffffffff") MessageBox.Show("White");
+            if (pixelColor.Name.ToString() == "ff000000") MessageBox.Show("Black");
+            bmp1.Save("C:\\test.png", ImageFormat.Png);
+            int test;
+        }
+    }
+}
